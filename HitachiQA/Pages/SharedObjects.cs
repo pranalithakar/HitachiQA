1dca518c5eff885011d93429bed360b5aa150509
Alex Caldarera <alex.caldarera@breakthroughfuel.com>
2022-03-16 17:30:44 -0400

    initial update

---
+++ HitachiQA/Pages/SharedObjects.cs
@@ -0,0 +1,15 @@
+﻿using HitachiQA.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HitachiQA.Pages
+{
+    public class SharedObjects
+    {
+        public static Element GetPublishedApp(string AppName)
+        {
+            return new Element($"//*[@role='listitem'][descendant::*[@title='{AppName}']]");
+        }
+    }
+}
