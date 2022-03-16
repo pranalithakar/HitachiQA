725dbb227b1e5600f71dda7a3ca6ca6eb05cfa53
Alex Caldarera <alex.caldarera@breakthroughfuel.com>
2022-03-16 18:25:47 -0400

    .

---
+++ HitachiQA/Features/AppLogin.feature
@@ -0,0 +1,6 @@
+﻿Feature: AppLogin
+	Field Service CRM Login
+
+@mytag
+Scenario: Add two numbers
+	Given user is landed on Apps page
---
+++ HitachiQA/StepDefinition/LoginSteps.cs
@@ -0,0 +1,17 @@
+﻿using HitachiQA.Driver;
+using HitachiQA.Helpers;
+using TechTalk.SpecFlow;
+
+namespace HitachiQA.StepDefinition
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"user is landed on Apps page")]
+        public void GivenUserIsLandedOnAppsPage()
+        {
+            UserActions.Navigate()
+        }
+
+    }
+}
