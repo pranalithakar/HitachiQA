710c69268cb926663d3c58cacf63fa9c7cc0d5f0
Alex Caldarera <alex.caldarera@breakthroughfuel.com>
2022-03-16 20:56:12 -0400

    update to steps

--- HitachiQA/StepDefinition/LoginSteps.cs
+++ HitachiQA/StepDefinition/LoginSteps.cs
@@ -29,11 +29,16 @@ namespace HitachiQA.StepDefinition
         public void GivenUserIsLandedOnAppsPage()
         {
             UserActions.Navigate("/");
-            SharedObjects.GetInputField("email").setText(Environment.GetEnvironmentVariable("USERNAME"));
+            var username = Environment.GetEnvironmentVariable("USER_NAME");
+            SharedObjects.GetInputField("email").setText(username);
             SharedObjects.GetButton("Next").Click();
             string password = Functions.DecryptString(Environment.GetEnvironmentVariable("PASSWORD"));
             SharedObjects.GetInputField("password").setText(password);
             SharedObjects.GetButton("Sign in").Click();
+            if(SharedObjects.GetButton("Yes").assertElementIsVisible(1, true) == true)
+            {
+                SharedObjects.GetButton("No").Click();
+            }
         }
 
     }
