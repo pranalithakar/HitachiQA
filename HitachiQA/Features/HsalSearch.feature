494e36a6228f79200444c9b082266cc000f1203f
Hitachi-QA <hponnala@hitachisolutions.com>
2022-04-25 14:33:55 -0400

    update for job search

--- HitachiQA/Features/HsalSearch.feature
+++ HitachiQA/Features/HsalSearch.feature
@@ -5,7 +5,17 @@
 
 @HSALSearch
 Scenario: Navigate to HSAL and search for automation
-  Given user landed on HSAL homepage
+	Given user landed on HSAL homepage
 	When user searches HSAL homepage for Automation
 	And user attempts to search HSAL homepage
-   Then user presented with search results from HSAL
+	Then user presented with search results from HSAL
+
+
+
+Scenario: HSAL job posting search
+	Given user landed on HSAL homepage
+	When navigation step
+	And search test
+	And click search button
+	Then we make sure search result text contains search test
+
---
+++ HitachiQA/StepDefinition/JobSearchSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace HitachiQA.StepDefinition
+{
+    [Binding]
+    internal class JobSearchSteps
+    {
+        [When(@"navigation step")]
+        public void WhenNavigationStep()
+        {
+            
+        }
+
+        [When(@"search test")]
+        public void WhenSearchTest()
+        {
+            
+        }
+
+        [When(@"click search button")]
+        public void WhenClickSearchButton()
+        {
+            
+        }
+
+        [Then(@"we make sure search result text contains search test")]
+        public void ThenWeMakeSureSearchResultTextContainsSearchTest()
+        {
+            
+        }
+    }
+}
